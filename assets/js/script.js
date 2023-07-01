eac4c8352ca8456f6d69964bfec0fea4e4975dd4
RadadiyaAbhay <abhay592004@gmail.com>
2023-07-01 16:21:46 +0530

    V.01.07.2023

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,41 @@ function search() {
   // Prevent form submission
   return false;
 }
+
+
+$(window).scroll(function(){
+  if ($(window).scrollTop() >= 100) {
+      $('.kai').addClass('head-after');
+
+  }
+  else {
+     $('.kai').removeClass('head-after');
+  }
+})
+
+
+
+function showSlide(slideIndex) {
+  var slides = document.getElementsByClassName("slider");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "block";
+}
+
+showSlide(0); 
+
+
+let currentUnderlineId = 'name1';
+
+function underlineName(nameId) {
+  const nameElement = document.getElementById(nameId);
+
+  if (currentUnderlineId !== null) {
+    const previousUnderlineElement = document.getElementById(currentUnderlineId);
+    previousUnderlineElement.classList.remove('underline');
+  }
+
+  nameElement.classList.add('underline');
+  currentUnderlineId = nameId;
+}
