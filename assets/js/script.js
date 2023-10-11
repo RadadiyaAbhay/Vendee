b87738aafe4226569955e42e42d82f519e06c6d1
RadadiyaAbhay <abhay592004@gmail.com>
2023-10-11 18:22:17 +0530

    V.11.10.23

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,36 +2,11 @@ function search() {
   // Retrieve the search query from the input field
   var query = document.getElementById("searchInput").value;
 
-  // Perform search operation (replace this with your own logic)
-  // For demonstration purposes, let's assume we have an array of data
-  var data = [
-    "mobiles",
-    "Consectetur adipiscing elit",
-    "Sed do eiusmod tempor incididunt",
-    "Ut labore et dolore magna aliqua"
-  ];
-
-  var results = data.filter(function(item) {
-    return item.toLowerCase().includes(query.toLowerCase());
-  });
-
-  // Display search results
-  var searchResultsDiv = document.getElementById("searchResults");
-  searchResultsDiv.innerHTML = ("#back");
-  if (results.length > 0) {
-    results.forEach(function(result) {
-      var resultElement = document.createElement("p");
-      resultElement.textContent = result;
-      searchResultsDiv.appendChild(resultElement);
-    });
-  } else {
-    var noResultsElement = document.createElement("p");
-    noResultsElement.textContent = "No results found.";
-    searchResultsDiv.appendChild(noResultsElement);
+  switch (query.toLowerCase()){
+    case mobile:
+        console.log("done");;
+        break;
   }
-
-  // Prevent form submission
-  return false;
 }
 
 
